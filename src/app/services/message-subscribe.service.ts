4122f954baf5cdcd892bac4c2969ad835280ad22
HungNguyen16 <72058424+HungNguyen16@users.noreply.github.com>
2020-12-07 10:12:30 +0700

    - remove message subscribe

--- src/app/services/message-subscribe.service.ts
+++ src/app/services/message-subscribe.service.ts
@@ -1,22 +1,22 @@
 import { Injectable } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
-import { MessageSubscribeData } from '../models/message-subscribe-data';
+// import { MessageSubscribeData } from '../models/message-subscribe-data';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MessageSubscribeService {
-  private subject = new Subject<MessageSubscribeData>();
+  // private subject = new Subject<MessageSubscribeData>();
 
-  sendMessage(messageData: MessageSubscribeData): void {
-      this.subject.next(messageData);
-  }
+  // sendMessage(messageData: MessageSubscribeData): void {
+  //     this.subject.next(messageData);
+  // }
 
-  clearMessages(): void {
-      this.subject.next(null);
-  }
+  // clearMessages(): void {
+  //     this.subject.next(null);
+  // }
 
-  getMessage(): Observable<MessageSubscribeData> {
-      return this.subject.asObservable();
-  }
+  // getMessage(): Observable<MessageSubscribeData> {
+  //     return this.subject.asObservable();
+  // }
 }
